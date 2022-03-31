7ab81f19187e2ff5323b4f64651f545b51522c35
sujin3 <elwkdlsj03@naver.com>
2022-03-31 13:55:21 +0900

    lotto app 90% 진행

--- public/script/lotto.js
+++ public/script/lotto.js
@@ -1,13 +1,15 @@
-//1게임 생성
+// 클릭할 대상
+const make1num = document.querySelector("#make_num1")
+const make5num = document.querySelector("#make_num5")
 
-//클릭할 대상
-var make5num = document.querySelector("#make_num5")
-
+// 이벤트 등록
+make1num.addEventListener("click", show1num);
 make5num.addEventListener("click", show5num);
 
-function show5num(){
-    var num_one = [];
-    for(var i=1; i<46; i++){
+// 로또 생성 함수
+function show1num(){
+    let num_one = [];
+    for(let i=1; i<46; i++){
         num_one.push(i);
     }
     num_one.sort(function(){
@@ -16,12 +18,15 @@ function show5num(){
     //console.log(num_one);
     //추출 .slice() vs .splice()
 
-    var start = Math.ceil(Math.random() * 40);
-    var new_num_one = num_one.slice(start, start+6);
-    console.log(new_num_one.sort(function(a,b){ //오름차순 정리
-        return a-b;
-    }));
-    var bonus_num = new_num_one[new_num_one.length-1]
-    console.log("보너스 번호 : "+bonus_num);
-    //localStorage.setItem("game_one", new_num_one)
+    let start = Math.ceil(Math.random() * 40);
+    let new_num_one = num_one.slice(start, start+6);
+    new_num_one.sort(function(a,b){ //오름차순 정리
+       return a-b;
+    });
+    //let bonus_num = new_num_one[new_num_one.length-1]
+    localStorage.setItem("lottoNum", new_num_one)
+     
+    location.href = "/result1"
 }
+
+//function show5num(){}
--- server.js
+++ server.js
@@ -5,28 +5,31 @@ const path = require('path')
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-//res.send('Hello World!')
   res.sendFile(path.join(__dirname, 'public'),'index.html')
 })
 
 app.get('/join', (req, res) => {
-//res.send('Hello World!')
   res.sendFile(__dirname+'/public/register.html')
 })
 
 app.get('/login', (req, res) => {
-//res.send('Hello World!')
   res.sendFile(__dirname+'/public/login.html')
 })
 
 app.get('/cs', (req, res) => {
-//res.send('Hello World!')
   res.sendFile(__dirname+'/public/customer.html')
 })
 
 app.get('/add_user', (req, res) => {
-//res.send('Hello World!')
   res.send("정상적으로 회원가입 되었습니다.")
+})
+
+app.get('/result1', (req, res) => {
+  res.sendFile(__dirname+'/public/result1.html')
+})
+
+app.get('/result5', (req, res) => {
+  res.sendFile(__dirname+'/public/result5.html')
 })
 
 app.listen(port, () => {
